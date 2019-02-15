ced11189f9fc6d383c449b35add2ca29f80a04dd
stoffeastrom <stoffeastrom@gmail.com>
2019-02-15 22:06:51 +0100

    fix: run IE

---
+++ test/component/aw.config.js
@@ -0,0 +1,13 @@
+const path = require("path");
+
+module.exports = {
+  capabilities: {
+    name: "desktop-chrome",
+    browserName: "internet explorer",
+    platform: "ANY",
+    version: "11"
+  },
+  specs: [path.resolve(__dirname, "test/hello.spec.js")],
+  artifactsPath: "test/component/__artifacts__",
+  seleniumArgs: [`-Dwebdriver.ie.driver=${process.env.IEWebDriver}`]
+};
--- test/index.spec.js
+++
@@ -1,7 +0,0 @@
-import getAzure from '../src/';
-
-describe('Azure', () => {
-  it('should work', () => {
-    expect(getAzure()).to.equal('azure');
-  })
-})
