90e012419a72d253de7b37156a60011ea87c3824
stoffeastrom <stoffeastrom@gmail.com>
2019-02-15 22:39:41 +0100

    fix: run IE

--- test/component/aw.config.js
+++ test/component/aw.config.js
@@ -1,5 +1,6 @@
 const path = require("path");
-console.error("IE WebDriver:", process.env.IEWebDriver);
+const IEWebDriver = `${process.env.IEWebDriver}\IEDriverServer.exe`;
+console.error("IE WebDriver:", IEWebDriver);
 module.exports = {
   capabilities: {
     name: "desktop-chrome",
@@ -10,9 +11,7 @@ module.exports = {
   specs: [path.resolve(__dirname, "test/hello.spec.js")],
   artifactsPath: "test/component/__artifacts__",
   localSeleniumStandaloneOpts: {
-    jvmArgs: [
-      `-Dwebdriver.ie.driver=${process.env.IEWebDriver}\IEDriverServer.exe`
-    ]
+    jvmArgs: [`-Dwebdriver.ie.driver=${IEWebDriver}`]
   },
   directConnect: false
 };
