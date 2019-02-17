f9f863b399494f5dd0cf1ee5719dc4a65ed25ae4
stoffeastrom <stoffeastrom@gmail.com>
2019-02-17 20:43:10 +0100

    fix: run IE

--- test/component/hello.spec.js
+++ test/component/hello.spec.js
@@ -1,6 +1,6 @@
 describe("Protractor", () => {
   it("should say hello world", async () => {
-    await browser.get("/examples/protractor/test/hello.fix.html");
+    await browser.get("/test/component/hello.fix.html");
     const container = await element(by.id("container"));
     const txt = await container.getText();
     expect(txt).to.equal("hello world");
