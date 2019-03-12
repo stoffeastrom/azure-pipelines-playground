7b89830411ba07f23db5bafa24a68c9593d2ff07
Christoffer Åström <cam@qlik.com>
2019-03-12 09:54:20 +0100

    chore: update

--- aw.config.js
+++ aw.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const IEWebDriver = `${process.env.IEWebDriver}\\IEDriverServer.exe`;
 // console.error('IE WebDriver:', IEWebDriver);
+console.error('PTOR', require.resolve('@after-work.js/protractor/src/plugins/reporter/aggregate'))
 module.exports = {
   capabilities: {
     name: 'desktop-ie',
@@ -9,7 +10,6 @@ module.exports = {
     version: '11'
   },
   specs: [path.resolve(__dirname, 'hello.spec.js')],
-  'filter.protractor.files': '**/test/component/**',
   artifactsPath: 'test/component/__artifacts__',
   localSeleniumStandaloneOpts: {
     jvmArgs: [`-Dwebdriver.ie.driver=${IEWebDriver}`]
